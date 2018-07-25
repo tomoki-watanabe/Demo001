8cb9980605df91fb1e812a4ba0bbbb15db298152
tomoki-watanabe <t-watanabe@kouei.co.jp>
2018-07-25 15:12:06 +0900

    yu

--- TestApp/Sample.cs
+++ TestApp/Sample.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         public static int DoSometing(int a, int b)
         {
-            var hoge = 9666;
+            var hoge = 965566;
             return a + b;
         }
     }
