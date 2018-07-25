24fabdf1e02e65bcafc330b271e251c72eb1c10e
tomoki.watanabe <t-watanabe@kouei.co.jp>
2018-07-25 11:24:01 +0900

    プロジェクト ファイルを追加します。

---
+++ TestApp/Sample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class Sample
+    {
+        public static int DoSometing(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TestAppTests/SampleTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Tests
+{
+    [TestClass()]
+    public class SampleTests
+    {
+        [TestMethod()]
+        public void DoSometingTest()
+        {
+            Assert.AreEqual(3, Sample.DoSometing(1, 2));
+        }
+    }
+}
